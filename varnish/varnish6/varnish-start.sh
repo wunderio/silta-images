94ee3b98a4f8c72c6d89c9f3985c4cc7c74c55f6
Jānis Bebrītis <janis.bebritis@wunder.io>
2021-06-09 16:23:03 +0300

    varnish: use /var/lib/varnish as working directory

--- varnish/varnish6/varnish-start.sh
+++ varnish/varnish6/varnish-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/sbin/varnishd -F -f /etc/varnish/default.vcl -T :6082 -S /etc/varnish/secret -s ${VARNISH_STORAGE_BACKEND:-'file,/var/lib/varnish/varnish_storage.bin,512M'} ${VARNISH_EXTRA_PARAMS}
+/usr/sbin/varnishd -F -f /etc/varnish/default.vcl -T :6082 -S /etc/varnish/secret -s ${VARNISH_STORAGE_BACKEND:-'file,/var/lib/varnish/varnish_storage.bin,512M'} -n /var/lib/varnish ${VARNISH_EXTRA_PARAMS}
