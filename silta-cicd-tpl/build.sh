88732547311f5a13f5abaa7b977568886817ce79
Rasmus Werling <rasmus.werling@gmail.com>
2024-04-23 12:25:07 +0300

    Switch to use the non-node version of the cimg/php image and install the node version we want ourselves.

--- silta-cicd-tpl/build.sh
+++ silta-cicd-tpl/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Builds the Docker images with different PHP and Node.js versions.
+# Currently, the following versions are supported:
+# - PHP versions 8.1 - 8.2
+# - Node.js versions 18 - 20
+# - Composer version 2
+
 set -eo pipefail
 
-docker build --build-arg="PHP_VERSION=8.1.23" . --tag circleci-php8.1-node18-composer2-v1.3.0 --tag circleci-php8.1-node18-composer2-v1.3 --tag circleci-php8.1-node18-composer2-v1
-docker build --build-arg="PHP_VERSION=8.2.12" . --tag circleci-php8.2-node18-composer2-v1.3.0 --tag circleci-php8.2-node18-composer2-v1.3 --tag circleci-php8.2-node18-composer2-v1
-docker build --build-arg="PHP_VERSION=8.2.13" . --tag circleci-php8.2-node20-composer2-v1.2.0 --tag circleci-php8.2-node20-composer2-v1.2 --tag circleci-php8.2-node20-composer2-v1
+NODE_18="18.20.2"
+NODE_20="20.12.2"
+
+docker build --build-arg="PHP_VERSION=8.1" --build-arg="NODE_VERSION=${NODE_18}" . --tag wunderio/silta-cicd:circleci-php8.1-node18-composer2-v1.3.0 --tag wunderio/silta-cicd:circleci-php8.1-node18-composer2-v1.3 --tag wunderio/silta-cicd:circleci-php8.1-node18-composer2-v1
+docker build --build-arg="PHP_VERSION=8.2" --build-arg="NODE_VERSION=${NODE_18}" . --tag wunderio/silta-cicd:circleci-php8.2-node18-composer2-v1.3.0 --tag wunderio/silta-cicd:circleci-php8.2-node18-composer2-v1.3 --tag wunderio/silta-cicd:circleci-php8.2-node18-composer2-v1
+docker build --build-arg="PHP_VERSION=8.2" --build-arg="NODE_VERSION=${NODE_20}" . --tag wunderio/silta-cicd:circleci-php8.2-node20-composer2-v1.2.0 --tag wunderio/silta-cicd:circleci-php8.2-node20-composer2-v1.2 --tag wunderio/silta-cicd:circleci-php8.2-node20-composer2-v1
