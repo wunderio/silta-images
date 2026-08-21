0f7214d856d3be10133d7809e1a470f381069845
Artis Krumins <artis.krumins@wunder.io>
2026-08-21 13:41:33 +0300

    Adding Mongo, removing debian13 images, making use of HC in DHI images

--- silta-redis/7.4-debian13/scripts/libbitnami.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Stub for Bitnami's libbitnami.sh — only needed if entrypoint.sh is used standalone
-
-# shellcheck disable=SC1091
-. /opt/bitnami/scripts/liblog.sh
-
-print_welcome_page() { :; }
-print_image_welcome_page() { :; }
